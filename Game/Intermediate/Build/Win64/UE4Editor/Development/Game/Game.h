f71da24ea104060892f4708d07128cee8d16f041
torihane <tobytobytbird@yahoo.co.jp>
2015-09-07 17:14:03 +0900

    【Bow and Arrow Action】 Splineの制御点の順番を表示できるようにした

--- Game/Intermediate/Build/Win64/UE4Editor/Development/Game/Game.h
+++ Game/Intermediate/Build/Win64/UE4Editor/Development/Game/Game.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_GAME_H__
 #define __AUTO_GAME_H__
-//Last Write: 2015/07/21 3:23:27 +00:00
-#include "C:\Users\game\Desktop\GraduationWorksGame\Game\Source\Game\Game.h"
+//Last Write: 2015/07/21 9:15:07 +00:00
+#include "C:\Users\pc-14\Desktop\GraduationWorksGame\Game\Source\Game\Game.h"
 #endif//__AUTO_GAME_H__
--- Game/Intermediate/Build/Win64/UE4Editor/Development/Game/PCH.Game.h.cpp
+++ Game/Intermediate/Build/Win64/UE4Editor/Development/Game/PCH.Game.h.cpp
@@ -1,1 +1,1 @@
-#include "C:\Users\game\Desktop\GraduationWorksGame\Game\Source\Game\Game.h"
+#include "C:\Users\pc-14\Desktop\GraduationWorksGame\Game\Source\Game\Game.h"
