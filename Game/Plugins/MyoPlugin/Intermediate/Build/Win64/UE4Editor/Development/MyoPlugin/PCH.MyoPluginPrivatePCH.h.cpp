001b28c0d53fed6decf37275b00913302366b99c
torihane <tobytobytbird@yahoo.co.jp>
2015-09-10 16:27:20 +0900

    【WiiRemote and MYO Calibrate】 ブループリントに変数が表示されるように修正

--- Game/Plugins/MyoPlugin/Intermediate/Build/Win64/UE4Editor/Development/MyoPlugin/PCH.MyoPluginPrivatePCH.h.cpp
+++ Game/Plugins/MyoPlugin/Intermediate/Build/Win64/UE4Editor/Development/MyoPlugin/PCH.MyoPluginPrivatePCH.h.cpp
@@ -1,1 +1,1 @@
-#include "C:\Users\game\Desktop\GraduationWorksGame\Game\Plugins\MyoPlugin\Source\MyoPlugin\Private\MyoPluginPrivatePCH.h"
+#include "C:\Users\pc-14\Desktop\GraduationWorksGame\Game\Plugins\MyoPlugin\Source\MyoPlugin\Private\MyoPluginPrivatePCH.h"
